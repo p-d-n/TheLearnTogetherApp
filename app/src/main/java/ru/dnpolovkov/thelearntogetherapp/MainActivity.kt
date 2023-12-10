8701abf86c63cfc80bbd759189de67cc05e71c58
Denis Polovkov <denis.polovkoff@gmail.com>
2023-12-10 21:40:38 +0300

    Added title and two article

--- app/src/main/java/ru/dnpolovkov/thelearntogetherapp/MainActivity.kt
+++ app/src/main/java/ru/dnpolovkov/thelearntogetherapp/MainActivity.kt
@@ -5,18 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.ContentScale.Companion.FillWidth
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.dnpolovkov.thelearntogetherapp.ui.theme.TheLearnTogetherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +33,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArticleImage()
+                    ArticleImage(
+                        stringResource(R.string.title_text,),
+                        stringResource(R.string.article_one),
+                        stringResource(R.string.article_second)
+                    )
                 }
             }
         }
@@ -37,23 +45,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArticleText(title: String, atirleOne: String, atirleSecond: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp,140.dp,16.dp,16.dp)
+        )
+        Text(
+            text = atirleOne,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp, 0.dp, 16.dp, 0.dp  )
+        )
+        Text(
+            text = atirleSecond,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp )
+        )
+    }
 }
 
 @Composable
-fun ArticleImage(modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Top,
-        modifier = modifier
-    ) {
+fun ArticleImage(title: String, atirleOne: String, atirleSecond: String) {
+    Box {
         Image(
             painter = painterResource(R.drawable.bg_compose_background),
             contentDescription = null,
             contentScale = FillWidth
+        )
+        ArticleText(
+            title = title,
+            atirleOne = atirleOne,
+            atirleSecond = atirleSecond
         )
     }
 }
@@ -62,6 +92,10 @@ fun ArticleImage(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TheLearnTogetherAppTheme {
-        ArticleImage()
+        ArticleImage(
+            stringResource(R.string.title_text),
+            stringResource(R.string.article_one),
+            stringResource(R.string.article_second)
+        )
     }
 }
