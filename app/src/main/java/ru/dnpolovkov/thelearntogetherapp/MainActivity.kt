3a85cb2f0ebb317a92ed4c4d4f4968e352833272
Denis Polovkov <denis.polovkoff@gmail.com>
2023-12-10 16:00:51 +0300

    Add image and composable function ArticleImage

--- app/src/main/java/ru/dnpolovkov/thelearntogetherapp/MainActivity.kt
+++ app/src/main/java/ru/dnpolovkov/thelearntogetherapp/MainActivity.kt
@@ -3,12 +3,17 @@ package ru.dnpolovkov.thelearntogetherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ContentScale.Companion.FillWidth
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import ru.dnpolovkov.thelearntogetherapp.ui.theme.TheLearnTogetherAppTheme
 
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArticleImage()
                 }
             }
         }
@@ -37,10 +42,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun ArticleImage() {
+    Image(
+        painter = painterResource(R.drawable.bg_compose_background),
+        contentDescription = null,
+        contentScale = FillWidth
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TheLearnTogetherAppTheme {
-        Greeting("Android")
+        ArticleImage()
     }
 }
